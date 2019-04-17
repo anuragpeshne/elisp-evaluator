28e408c55e927a7a25c5314f69d675a2c1c9b273
Anurag Peshne <anurag.peshne@gmail.com>
2019-04-16 22:05:37 -0700

    evaluates nested sexp

--- src/elisp_evaluator/evaluator.clj
+++ src/elisp_evaluator/evaluator.clj
@@ -1,17 +1,27 @@
 (ns elisp-evaluator.evaluator)
 
+(declare eval-ast)
+
 (defn empty-environment
   "Returns an empty environment."
   []
   {
    "+" (fn [ops] (apply + ops))
-   "*" (fn [ops] (apply + ops))
+   "*" (fn [ops] (apply * ops))
    })
+
+(defn- eval-atom
+  "Evaluates an atom in the given environment."
+  [atom, env]
+  (if (vector? atom)
+    (eval-ast atom env)
+    (cond
+      (number? atom) atom
+      (contains? env atom) (get env atom)
+      :else (throw (Exception. (str "Unknown symbol " atom))))))
 
 (defn eval-ast
   "Evaluates AST in the given environment."
   [ast-exp, env]
-  (let [[operator & operands] ast-exp]
-    (if (contains? env operator)
-      ((get env operator) operands)
-      (throw (Exception. (str "Unknown symbol " operator))))))
+  (let [[operator & operands] (map #(eval-atom % env) ast-exp)]
+    (operator operands)))
