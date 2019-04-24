8a93cef57188ce4ce50b6a8a48810b96a91b8e57
anuragpeshne <anurag.peshne@gmail.com>
2019-04-23 23:40:58 -0700

    implements `defvar`, returns new env after each operation

--- src/elisp_evaluator/evaluator.clj
+++ src/elisp_evaluator/evaluator.clj
@@ -6,22 +6,23 @@
   "Returns an empty environment."
   []
   {
-   "+" (fn [ops] (apply + ops))
-   "*" (fn [ops] (apply * ops))
+   "+" (fn [env, ops] [env, (apply + ops)])
+   "*" (fn [env, ops] [env, (apply * ops)])
+   "defvar" (fn [env, [var, val]] [(assoc env var val), nil])
    })
 
 (defn- eval-atom
   "Evaluates an atom in the given environment."
   [atom, env]
-  (if (vector? atom)
-    (eval-ast atom env)
-    (cond
-      (number? atom) atom
-      (contains? env atom) (get env atom)
-      :else (throw (Exception. (str "Unknown symbol " atom))))))
+  (cond
+    (vector? atom) (let [[env, result] (eval-ast atom env)] result)
+    (number? atom) atom
+    (contains? env atom) (get env atom)
+    (string? atom) atom
+    :else (throw (Exception. (str "elisp error: Unknown symbol " atom)))))
 
 (defn eval-ast
   "Evaluates AST in the given environment."
   [ast-exp, env]
-  (let [[operator & operands] (map #(eval-atom % env) ast-exp)]
-    (operator operands)))
+  (let [[operator & operands] (map #(eval-atom %1 env) ast-exp)]
+    (operator env operands)))
--- test/elisp_evaluator/evaluator_test.clj
+++ test/elisp_evaluator/evaluator_test.clj
@@ -12,12 +12,29 @@
     (let [input-output-list ['("(+ 1 2)" 3)
                              '("(* 2 2)" 4)
                              '("(+ 2 (* 2 3))" 8)]]
-      (loop [i-o input-output-list]
+      (loop [i-o input-output-list
+             env (empty-environment)]
         (if (not (empty? i-o))
           (let [[input expected-output] (first i-o)
-                output (-> input
-                           tokenize
-                           parse
-                           (eval-ast (empty-environment)))]
+                [env, output] (-> input
+                                  tokenize
+                                  parse
+                                  (eval-ast env))]
             (is (= expected-output output))
-            (recur (rest i-o))))))))
+            (recur (rest i-o) env)))))))
+
+(deftest eval-test-defvar
+  (testing "defvar test."
+    (let [input-output-list ['("(defvar a 12)" "a" 12 nil)]]
+      (loop [i-o input-output-list
+             env (empty-environment)]
+        (if (not (empty? i-o))
+          (let [[input key value expected-output] (first i-o)
+                [env, output] (-> input
+                                  tokenize
+                                  parse
+                                  (eval-ast env))]
+            (is (and (= expected-output output)
+                     (contains? env key)
+                     (= (get env key) value)))
+            (recur (rest i-o) env)))))))
